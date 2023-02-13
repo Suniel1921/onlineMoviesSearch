76330c2fb17d1237d30bf1192f7253c5fd2738c6
sunil <logins697@gmail.com>
2023-02-13 18:13:42 +0545

    initialized

---
+++ index.js
@@ -0,0 +1,46 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const moviesBox = document.querySelector('.movie-box');
+
+const getMovies = async (api)=>{
+    const response = await fetch(api)
+    const data = await response.json();
+    // console.log(data)
+    showMovies(data.results)
+}
+
+
+const showMovies = (data)=>{
+    moviesBox.innerHTML = "";
+    data.forEach(
+        (item)=>{
+            const box = document.createElement("div");
+            box.classList.add("box");
+            box.innerHTML = `
+
+            <img src="${IMGPATH + item.poster_path}" alt="">
+            <div class="content">
+                <h3>${item.original_title}</h3>
+                <p>${item.overview}</p>
+            </div>
+            `;
+            moviesBox.appendChild(box)
+
+        }
+    )
+}
+
+document.querySelector("#search").addEventListener(
+    "keyup",
+    function(event){
+        if(event.target.value != ""){
+            getMovies(SEARCHAPI + event.target.value);
+        }else{
+            getMovies(APIURL);
+        }
+    }
+)
+
+getMovies(APIURL)
